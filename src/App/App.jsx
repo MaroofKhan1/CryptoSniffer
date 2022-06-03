6ac060fa4a757ff7098d5ccc81fade00c0d1dde8
maroofkhan <khan.maroof6569@gmail.com>
2022-06-03 17:19:44 -0500

    first commit

---
+++ src/App/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import './App.css';
+
+
+function App() {
+  const [user, setUser] =useState(null);
+  return (
+    <main className="App">
+      App
+    </main>
+  );
+}
+
+export default App;
